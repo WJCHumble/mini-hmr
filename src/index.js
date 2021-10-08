f94c5be700c3f1bdd26e034e88e3cb0eff6e363b
wujingchang <1957503379@qq.com>
2021-10-08 14:49:30 +0800

    feat: add file watcher

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 const http = require("http")
-const WebSocket = require("ws")
+const { WebSocket } = require("ws")
+const chokidar = require('chokidar');
+const path = require("path")
+const fs = require("fs")
 
 function createWebSocketServer() {
-	const wss = new WebSocket.Server({
-		port: 24678,
-	})
-
+	const wss = new WebSocket.Server({ noServer: true })
 	wss.on("connection", (socket) => {
 		socket.send(JSON.stringify({ type: "connected" }))
 		console.log("ws")
@@ -16,16 +16,34 @@ function createWebSocketServer() {
 			console.error(e.message)
 		}
 	})
+
+	return {
+		send(payload) {
+			const stringified = JSON.stringify(payload)
+      wss.clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN) {
+          client.send(stringified)
+        }
+      })
+		}
+	}
 }
 
 function createServer() {
-	const httpServer = http.createServer()
+	const httpServer = http.createServer(function(request, response) {
+		const file = fs.readFileSync(path.join(__dirname, "../example/index.html"))
+		response.writeHead(200, {'Content-Type': "text/html"})
+		response.end(file)
+	})
+	const wss = createWebSocketServer()
 
-	createWebSocketServer()
-	// const socket = new WebSocket("wss://localhost:24678", "min-hmr")
-	// socket.addEventListener("message", async ({ data }) => {
-	// 	console.log(JSON.parse(data))
-	// })
+	const watcher = chokidar.watch(path.join(__dirname, "../example/"))
+	watcher.on("change", async (file) => {
+		wss.send({
+			type: "full-reload",
+			updates: [file]
+		})
+	})
 
 	httpServer.listen(3000)
 	console.log("[min-hmr] server connected.")
